2fda5fae84c13c26aafb4dc860c4e6cff14f21bd
perfstranger <sali.kachibaia@gmail.com>
2017-07-03 00:08:39 +0400

    form image uploader
    სტილები და რესფონსიული ვარიანტი გასასწორებელია

--- js/script.js
+++ js/script.js
@@ -3,3 +3,18 @@ $(document).ready(function() {
     $(this).addClass('active').siblings().removeClass('active');
   });
 });﻿
+
+$('#fileUpload').on('change',function(){
+  $('.avatar').removeClass('open');
+});
+$('.avatar').on('click',function(){
+  $(this).addClass('open');
+});
+// added code to close the modal if you click outside
+$('html').click(function() {
+ $('.avatar').removeClass('open');
+});
+
+$('.avatar').click(function(event){
+    event.stopPropagation();
+});
